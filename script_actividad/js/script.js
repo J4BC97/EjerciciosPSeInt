5aceae6bc5a589b72705ef825dfeabeb246da5e0
J4BC97 <hammerban123@gmail.com>
2024-10-07 09:19:40 -0500

    Trabajo Script JS

---
+++ script_actividad/js/script.js
@@ -0,0 +1,21 @@
+//Script trabajo Prompt
+
+//Variables
+
+var ciudad = "Cali";
+
+var usuarioNombre = prompt("Por favor ingrese su nombre");
+var usuarioEdad = prompt("Digite su edad");
+
+//Mostrando en la consola
+
+console.log("Bienvenido a esta pequeña prueba");
+
+//Mensaje de Bienvenida al usuario en ventana emergente"
+
+alert(" Bienvenido " + usuarioNombre);
+
+//Datos de variables mostrados en consola
+
+console.log("Hola " + usuarioNombre + "," + " tienes " + usuarioEdad + " años y vives en la ciudad de " + ciudad );
+
