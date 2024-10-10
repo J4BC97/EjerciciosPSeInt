cde81440930cf680d00fb47b1a92e3cab235b9ab
J4BC97 <hammerban123@gmail.com>
2024-10-10 08:53:34 -0500

    Script: Contador de Vocales JS

---
+++ script_contador_vocales/js/contador_de_vocales.js
@@ -0,0 +1,17 @@
+//Practica JS: Contador de vocales
+
+
+
+let usuarioFrase = prompt("Por favor, escriba alguna frase");
+let vocales = 'aeiouaeiouáéíóúAEIOUÁÉÍÓÚ';
+let arregloVocales = [];
+
+for (let contador = 0; contador < usuarioFrase.length; contador++) {
+    let letra = usuarioFrase[contador];
+    if (vocales.includes(letra)) {
+        arregloVocales.push(letra);
+    }
+    console.log(`La frase ingresada contiene: ${contador}`);
+    console.log(`Las vocales contadas son: ${arregloVocales}`);
+}
+
